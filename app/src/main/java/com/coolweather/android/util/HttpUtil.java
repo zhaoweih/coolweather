85975c0ed33e9005521aca073fb5f16b3ff558f4
Tony <tony@gmail.com>
2017-02-02 16:57:53 +0800

    some change.

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Administrator on 2017/2/2.
+ */
+public class HttpUtil  {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client =new OkHttpClient();
+        
+    }
+}
